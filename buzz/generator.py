b430db5c437d3dbf2b76d46f0f09a248e81a0c40
Arun Singh <singh.arun@machinepulse.com>
2018-02-01 12:19:29 +0530

    added comment to gen python file

--- buzz/generator.py
+++ buzz/generator.py
@@ -14,6 +14,7 @@ def sample(l, n = 1):
         return result[0]
     return result
 
+# random generate function
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
